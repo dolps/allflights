bc3fc298cbd26d021e205823b510e1cc0241aed4
thomasdolplads <thomas.dolplads@twoday.com>
2025-12-05 15:35:10 +0100

    fix leaflet

--- src/components/Leaflet.jsx
+++ src/components/Leaflet.jsx
@@ -2,6 +2,8 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Leaflet = ({ mapAttributes }) => {
+    console.log('mapattributes:: ', JSON.stringify(mapAttributes))
+    const position = [51.505, -0.09]
     return (
         <MapContainer center={mapAttributes.startPosition} zoom={12} scrollWheelZoom={false} zoomControl={false}
             dragging={false}
@@ -12,7 +14,7 @@ const Leaflet = ({ mapAttributes }) => {
                 url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
                 attribution='Tiles &copy; Esri'
             />
-            <Marker position={position}>
+            <Marker position={mapAttributes.startPosition}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
